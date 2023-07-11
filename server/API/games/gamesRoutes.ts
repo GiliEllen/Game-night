a593e267707a42868817d9560aa02e5a2503b555
Gili <gilimena95@gmail.com>
2023-07-11 10:41:19 +0300

    Update gamesRoutes.ts

--- server/API/games/gamesRoutes.ts
+++ server/API/games/gamesRoutes.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import { 
-    // findGameByUser,
+    findGameByUser,
      findGameByName,
       addGame,
-    //    addGameToUser,
-    //  getAllGames 
+       addGameToUser,
+     getAllGames 
     } from "./gamesCont";
 
 
 const router = express.Router();
 
 router
-// .post("/get-all-games-by-user", getAllGames)
-// .post("/find-game-by-user", findGameByUser)
+.post("/get-all-games-by-user", getAllGames)
+.post("/find-game-by-user", findGameByUser)
 .post("/find-game-by-name", findGameByName)
 .post("/Add-New-Game", addGame)
-// .post("/add-game-to-user", addGameToUser)
+.post("/add-game-to-user", addGameToUser)
 
 export default router;
