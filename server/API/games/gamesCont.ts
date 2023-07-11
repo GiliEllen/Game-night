92e9138d6cbf839e64fb6816c2f1694e35c075a6
Gili <gilimena95@gmail.com>
2023-07-11 10:23:15 +0300

    add game to user

--- server/API/games/gamesCont.ts
+++ server/API/games/gamesCont.ts
@@ -112,92 +112,87 @@ export async function addGame(req: express.Request, res: express.Response) {
   }
 }
 
-// export async function addGameToUser(
-//   req: express.Request,
-//   res: express.Response
-// ) {
-//   try {
+export async function addGameToUser(
+  req: express.Request,
+  res: express.Response
+) {
+  try {
 
-//     const secret = process.env.JWT_SECRET;
-//     if (!secret) throw new Error("Couldn't load secret from .env");
-//     const { userId } = req.cookies;
-//     if (!userId) throw new Error("no userId found");
-//     if (userId === undefined) throw new Error("no user");
+    const secret = process.env.JWT_SECRET;
+    if (!secret) throw new Error("Couldn't load secret from .env");
+    const { userID } = req.cookies;
+    if (!userID) throw new Error("no userId found");
+    if (userID === undefined) throw new Error("no user");
 
-//     const decodedUserId = jwt.decode(userId, secret);
-//     const { userID } = decodedUserId;
-//     const { name } = req.body;
-//     if (!userID || !name)
-//       throw new Error("no loggedInUser or name from client on addGameToUser");
-//     const query = `INSERT INTO gamenight.users_games (user_owner_id ,game_id)
-//     SELECT "${userID}", game_id FROM gamenight.games
-//     WHERE game_name = '${name}';`;
+    const decodedUserId = jwt.decode(userID, secret);
+    const { userId } = decodedUserId;
+    const { name } = req.body;
+    if (!userId || !name)
+      throw new Error("no loggedInUser or name from client on addGameToUser");
 
-//     db.query(query, (err, results, fields) => {
-//       try {
-//         if (err) throw err;
-//         res.send({ results });
-//       } catch (error) {
-//         console.log(err);
-//         res.status(500).send({ ok: false, error: err });
-//       }
-//     });
-//   } catch (error) {
-//     res.status(500).send({ error: error });
-//   }
-// }
+    const gameDB = await GameModel.findOne({gameName: name})
+    if(!gameDB) throw new Error("no game found with this name")
 
-// export async function getAllGames(req: express.Request, res: express.Response) {
-//   try {
-//     const secret = process.env.JWT_SECRET;
-//     if (!secret) throw new Error("Couldn't load secret from .env");
-//     const { userId } = req.cookies;
-//     if (!userId) throw new Error("no userId found");
-//     if (userId === undefined) throw new Error("no user");
+    const userGameDB = await UserGameModel.create({gameId: gameDB._id, userId})
 
-//     const decodedUserId = jwt.decode(userId, secret);
-//     const { userID } = decodedUserId;
-//     if (!userId || !userID)
-//       throw new Error("no loggedInUser from client on get all games");
-//     const query = `SELECT *
-//     FROM gamenight.games as g;
-//     SELECT g.game_name, g.game_img, g.game_id
-//     FROM games as g
-//     JOIN users_games as ug ON g.game_id = ug.game_id
-//     JOIN users as u ON u.user_id = ug.user_owner_id
-//     WHERE u.user_id = "${userID}";`;
+    res.send({ results: userGameDB });
+    
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+}
 
-//     db.query(query, [1,2], (err, results, fields) => {
-//       try {
-//         if (err) throw err;
-//         const gamesArray = [];
-//         results[0].map((result) => {
+export async function getAllGames(req: express.Request, res: express.Response) {
+  try {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) throw new Error("Couldn't load secret from .env");
+    const { userId } = req.cookies;
+    if (!userId) throw new Error("no userId found");
+    if (userId === undefined) throw new Error("no user");
 
-//           if (results[1].some(e => e.game_id === result.game_id)) {
-//             gamesArray.push({
-//               game_name: result.game_name,
-//               game_img: result.game_img,
-//               game_id: result.game_id,
-//               gameAddble: false,
-//             });}
-//            else {
-//             gamesArray.push({
-//               game_name: result.game_name,
-//               game_img: result.game_img,
-//               game_id: result.game_id,
-//               gameAddble: true,
-//             });
-//           }
-//         });
-//         res.send({ gamesArray });
-//       } catch (error) {
-//         console.log(err, error);
-//         res.status(500).send({ ok: false, error: err, error2: error });
-//       }
-//     });
-//   } catch (error) {
-//     console.log( error);
+    const decodedUserId = jwt.decode(userId, secret);
+    const { userID } = decodedUserId;
+    if (!userId || !userID)
+      throw new Error("no loggedInUser from client on get all games");
+    const query = `SELECT *
+    FROM gamenight.games as g;
+    SELECT g.game_name, g.game_img, g.game_id
+    FROM games as g
+    JOIN users_games as ug ON g.game_id = ug.game_id
+    JOIN users as u ON u.user_id = ug.user_owner_id
+    WHERE u.user_id = "${userID}";`;
 
-//     res.status(500).send({ error: error });
-//   }
-// }
+    db.query(query, [1,2], (err, results, fields) => {
+      try {
+        if (err) throw err;
+        const gamesArray = [];
+        results[0].map((result) => {
+
+          if (results[1].some(e => e.game_id === result.game_id)) {
+            gamesArray.push({
+              game_name: result.game_name,
+              game_img: result.game_img,
+              game_id: result.game_id,
+              gameAddble: false,
+            });}
+           else {
+            gamesArray.push({
+              game_name: result.game_name,
+              game_img: result.game_img,
+              game_id: result.game_id,
+              gameAddble: true,
+            });
+          }
+        });
+        res.send({ gamesArray });
+      } catch (error) {
+        console.log(err, error);
+        res.status(500).send({ ok: false, error: err, error2: error });
+      }
+    });
+  } catch (error) {
+    console.log( error);
+
+    res.status(500).send({ error: error });
+  }
+}
